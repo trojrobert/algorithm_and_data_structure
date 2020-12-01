dadc36aca32fe5afb1d97ed91a0244cda31712c6
trojrobert <trojrobert@gmail.com>
2020-12-01 07:24:02 -0500

    add reverse.py file

---
+++ DataStructuresArrays/reverse.py
@@ -0,0 +1,26 @@
+
+def reverse(string_item):
+
+    #check for instance type 
+
+    if isinstance(string_item, str) and len(string_item) < 2:
+
+        return "Input is not a string"
+
+    backward_input = []
+
+    for i in range(len(string_item)-1, -1, -1):
+        backward_input.append(string_item[i])
+    
+
+    backward_input = "".join(backward_input)
+   
+
+    #return backward_input
+
+    return string_item[::-1]
+
+
+if __name__ == "__main__":
+
+  print(reverse("I went to Stuttgart"))
